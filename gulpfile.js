df9364e06edaaa096bc77c094411f33a6e3ad357
vanderlanth <nicolaslanthemann@gmail.com>
2015-07-29 15:27:35 +0200

    browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,54 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var jade = require('gulp-jade');
+var browserSync = require('browser-sync').create();
+
+// gulp.task('serve', function() {
+//     browserSync.init({
+//         server: './build'
+//     });
+//     gulp.watch('src/*.scss',['sass']);
+//     gulp.watch('src/*.jade',['jade']);
+//     gulp.watch('src/*.js',['scripts']);
+//     gulp.watch("build/*.css").on('change', browserSync.reload);
+//     gulp.watch("build/*.html").on('change', browserSync.reload);
+//     gulp.watch("build/*.js").on('change', browserSync.reload);
+// });
 
 
-gulp.task('styles', function() {
-    gulp.src('src/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('build/'))
+gulp.task('watch', function(gulpCallback) {
+  browserSync.init({
+    server: './build/',
+    open: true
+  }, function callback() {
+    gulp.watch('build/*.html', browserSync.reload);
+    gulp.watch('build/*.css', browserSync.reload);
+    gulp.watch('build/*.js', browserSync.reload);
+    gulp.watch('src/*.scss', ['sass']);
+    gulp.watch('src/*.jade',['jade']);
+    gulp.watch('src/*.js',['scripts']);
+    gulpCallback();
+  });
 });
 
- 
-gulp.task('jade', function() {
-  gulp.src('src/*.jade')
-  	.pipe(jade())
+gulp.task('sass', function() {
+  return gulp.src('src/*.scss')
+    .pipe(sass())
     .pipe(gulp.dest('build/'))
+    .pipe(browserSync.stream());
 });
 
-
+gulp.task('jade', function() {
+     return gulp.src('src/*.jade')
+    .pipe(jade())
+    .pipe(gulp.dest('build/'))
+    .pipe(browserSync.stream());
+});
 
 gulp.task('scripts', function() {
-  gulp.src('src/*.js')
+     return gulp.src('src/*.js')
     .pipe(gulp.dest('build/'))
+    .pipe(browserSync.stream());
 });
 
-
-//Watch task
-gulp.task('default', ['jade', 'styles', 'scripts'], function() {
-    gulp.watch('src/*.scss',['styles']);
-    gulp.watch('src/*.jade',['jade']);
-});
+gulp.task('default', ['watch']);
